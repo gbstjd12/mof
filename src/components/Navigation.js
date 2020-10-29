9a5c2b0c9ebfdb30c82de9eea2e4d1e4791e6899
gbstjd12 <gbstjd12@naver.com>
2020-10-29 14:14:46 +0900

    최종 완성 코드

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Navigation.css';
+
+
+function Navigation(){
+    return(
+        <div className='nav'>
+            <Link to="/">영화 홈</Link>
+            <Link to="/about">만든이</Link>
+        </div>
+    )
+}
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root')); //App.js에서 작성한 코드를 index.html의 아이디가 'root'인 엘리먼트에 넣어줌
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
